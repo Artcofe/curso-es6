a3ea71942a19707ef720a52681f6f0cff202ae0b
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-06-19 20:05:36 +0000

    improve javadoc

--- src/components/ChatBox/index.tsx
+++ src/components/ChatBox/index.tsx
@@ -1,3 +1,13 @@
 import React, { useMemo } from 'react';
 import RabbitHappy from '@site/static/img/characters/rabbit/rabbit-happy.png';
-import RaccoonDizzy from '@site/static/img/characters/raccoon/raccoon
+import RaccoonDizzy from '@site/static/img/characters/raccoon/raccoon-dizzy.png';
+import styled from '@emotion/styled';
+
+type Avatar = 'rabbit-happy' | 'raccoon-dizzy';
+
+const ChatBox = ({
+  avatar,
+  children,
+}: {
+  avatar: Avatar;
+  children: React
