5ff5bfb340811c5cc8683062a859549772e8c6c1
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-06-24 08:36:06 +0000

    refresh defconfigs

--- src/components/HomepageFeatures.tsx
+++ src/components/HomepageFeatures.tsx
@@ -1,3 +1,9 @@
 import React from 'react';
 import { css } from '@emotion/react';
-import styled from '@
+import styled from '@emotion/styled';
+import mq from 'theme/mq';
+
+const FeatureList = [
+  {
+    title: 'DAU, GA... 먹는건가요?',
+    Svg: require('../../static/img/undraw_docusaurus_mountain.svg')
