382588f163fe71ec8172e21dae05a7cd69f1ad36
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-06-28 22:55:28 +0000

    src/Builder4/WindowLeftProps.bjs src/Builder4/WindowLeftProps.vala

--- src/components/HomepageFeatures.tsx
+++ src/components/HomepageFeatures.tsx
@@ -6,4 +6,10 @@ import mq from 'theme/mq';
 const FeatureList = [
   {
     title: 'DAU, GA... 먹는건가요?',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg')
+    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        사용자 데이터 분석(Daily Active User, 전환율 측정 등)은 모던 웹
+        프로젝트에서 필수적인 부분이지만, 어디서부터 시작해야 할지 막막해하는
+        개발자들이 많습니다. 이 핸드북을 통해 기초적인 분석에 대한 개념을
+    
