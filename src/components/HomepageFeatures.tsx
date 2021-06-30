adfceba2eb24e973575e551e65778b339a78cd33
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-06-30 20:48:34 +0000

    Fix typo in variable name

--- src/components/HomepageFeatures.tsx
+++ src/components/HomepageFeatures.tsx
@@ -12,4 +12,13 @@ const FeatureList = [
         사용자 데이터 분석(Daily Active User, 전환율 측정 등)은 모던 웹
         프로젝트에서 필수적인 부분이지만, 어디서부터 시작해야 할지 막막해하는
         개발자들이 많습니다. 이 핸드북을 통해 기초적인 분석에 대한 개념을
-    
+        잡습니다.
+      </>
+    ),
+  },
+  {
+    title: '개발자에게 필요한 웹 분석 지식',
+    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        여러분의 프로젝트에 웹 분석 툴을 연동하
