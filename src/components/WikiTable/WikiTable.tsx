910c809d8be57e1f4d3fc91b19ec63a942e014d7
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-09-03 17:40:18 +0000

    history

--- src/components/WikiTable/WikiTable.tsx
+++ src/components/WikiTable/WikiTable.tsx
@@ -1,1 +1,8 @@
-import React, { useEffect } fr
+import React, { useEffect } from 'react';
+
+import WikiTableRow from './WikiTableRow';
+
+import usePagination from '../../hooks/usePagination';
+
+export default function WikiTable({ words = [] }: { words: string[] }) {
+  const { 
