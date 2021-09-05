75d4486b769938b95acd63628fc892bf23794743
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-09-05 09:16:08 +0000

    Commit from Sugar Labs: Translation System by user cjl.: 40 of 40 messages translated (0 fuzzy).

--- src/components/WikiTable/WikiTable.tsx
+++ src/components/WikiTable/WikiTable.tsx
@@ -5,4 +5,12 @@ import WikiTableRow from './WikiTableRow';
 import usePagination from '../../hooks/usePagination';
 
 export default function WikiTable({ words = [] }: { words: string[] }) {
-  const { 
+  const { onPrevious, onNext, currentPage, result, isLastPage, isFirstPage } =
+    usePagination({
+      source: words,
+      offset: 2,
+    });
+
+  return (
+    <>
+      <span>{currentPage}</span>
