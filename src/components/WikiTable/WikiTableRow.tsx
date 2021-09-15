984c9dbe16b38d4d36e39344157a7fc582cc18ec
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-09-15 16:33:02 +0000

    Add migrations

--- src/components/WikiTable/WikiTableRow.tsx
+++ src/components/WikiTable/WikiTableRow.tsx
@@ -5,4 +5,8 @@ import { WikiWord } from '../../types';
 export default function WikiTableRow({ name, description }: WikiWord) {
   return (
     <tr>
-      <td>{name
+      <td>{name}</td>
+      <td>{description}</td>
+    </tr>
+  );
+}
