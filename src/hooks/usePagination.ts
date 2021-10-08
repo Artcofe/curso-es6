c6337b12a95fcbcbcfb877c87543c3455f9c68c3
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-10-08 09:43:00 +0000

    logic directive

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,3 +1,7 @@
 import { useEffect, useState } from 'react';
 
-const usePagination = ({ source = [
+const usePagination = ({ source = [], initialPage = 1, offset = 10 }) => {
+  const getResult = () => {
+    const startIdx = (currentPage - 1) * offset;
+    const endIdx = startIdx + offset;
+    return source.slice(startI
