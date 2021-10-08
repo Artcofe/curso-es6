d9a946aa0627cf2f78db34872b59ac9b21ec7642
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-10-08 14:32:29 +0000

    util

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -4,4 +4,12 @@ const usePagination = ({ source = [], initialPage = 1, offset = 10 }) => {
   const getResult = () => {
     const startIdx = (currentPage - 1) * offset;
     const endIdx = startIdx + offset;
-    return source.slice(startI
+    return source.slice(startIdx, endIdx);
+  };
+
+  const getMaxPage = () => {
+    return Math.max(Math.ceil(source.length / offset), 1);
+  };
+
+  const minPage = 1;
+  const [currentPage, setCurrentPage] = useS
