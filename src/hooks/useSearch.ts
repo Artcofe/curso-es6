d31455c70337d022bde4160aad35a21c92b44d1b
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-10-13 15:54:52 +0000

    small ui fix

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -3,4 +3,11 @@ import { WikiWord } from '../types';
 import KOREAN_CONSONANTS from '../../static/korean-consonants';
 
 const UNICODE_FIRST_FOREAN = 44032;
-const NUMBER_OF_KOREAN_LETTERS 
+const NUMBER_OF_KOREAN_LETTERS = 11172;
+const NUMBER_OF_KOREAN_NUCLEUS = 21;
+const NUMBER_OF_KOREAN_CODA = 28;
+
+const ALPHABET_REGEX = new RegExp(/^[A-Za-z]$/);
+const KOREAN_CONSONANTS_REGEX = new RegExp(/^[ㄱ-ㅎ]$/);
+
+const 
