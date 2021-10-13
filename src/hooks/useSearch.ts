1ced2c0c8ae95323d527e3ed7a92767eb5a7b22d
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-10-13 15:05:31 +0000

    update docker-image-resource blob

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,1 +1,6 @@
-import { useS
+import { useState } from 'react';
+import { WikiWord } from '../types';
+import KOREAN_CONSONANTS from '../../static/korean-consonants';
+
+const UNICODE_FIRST_FOREAN = 44032;
+const NUMBER_OF_KOREAN_LETTERS 
