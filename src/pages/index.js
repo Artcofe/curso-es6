d4d4c548086aee606f3b5cac5081523bcf943e3b
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-11-13 13:24:42 +0000

    devel commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,11 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css
+import styles from './index.module.css';
+import HomepageFeatures from 'components/HomepageFeatures';
+import Footer from 'theme/Footer';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <hea
