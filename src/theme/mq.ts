cc93317c55ff62e572a638223d381ab46f4be590
Artcofe <94559872+Artcofe@users.noreply.github.com>
2021-12-04 08:39:33 +0000

    ASoC: wm8955: Fix setting wrong register for WM8955_K_8_0_MASK bits

--- src/theme/mq.ts
+++ src/theme/mq.ts
@@ -1,1 +1,13 @@
-import facepaint, { DynamicStyleFunction } f
+import facepaint, { DynamicStyleFunction } from 'facepaint';
+
+type DeviceSize = 'm';
+
+type DeviceWidth = {
+  [key in DeviceSize]: number;
+};
+
+const deviceWidth: DeviceWidth = {
+  m: 1000,
+};
+
+const mq = (breakpoints = dev
